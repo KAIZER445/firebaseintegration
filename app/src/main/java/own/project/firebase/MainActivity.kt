c23dcfd8933a6b66958c43c120f4f20d3bca479e
KAIZER445 <prashanttamang861@gmail.com>
2025-08-31 10:15:22 +0545

    reading dat ain real time

--- app/src/main/java/own/project/firebase/MainActivity.kt
+++ app/src/main/java/own/project/firebase/MainActivity.kt
@@ -3,16 +3,22 @@ package own.project.firebase
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : androidx.appcompat.app.AppCompatActivity() {
 
     lateinit var editTextName: EditText
     lateinit var buttonSend: Button
+    lateinit var textViewName: TextView
 
     val database: FirebaseDatabase = FirebaseDatabase.getInstance()
     val reference: DatabaseReference = database.reference.child("users")
+    val reference2: DatabaseReference = database.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +26,20 @@ class MainActivity : androidx.appcompat.app.AppCompatActivity() {
 
         editTextName = findViewById(R.id.editTextName)
         buttonSend = findViewById(R.id.buttonSend)
+        textViewName = findViewById(R.id.textViewName)
+
+        reference2.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val realName: String = snapshot.child("users").child("name").value as String
+                textViewName.text = realName
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
 
         buttonSend.setOnClickListener {
             val userName = editTextName.text.toString()
